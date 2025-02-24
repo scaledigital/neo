89bdb8eef224a0d48d8329705a0dcb3a21798a85
scaledigital <scaledigital@proton.me>
2025-02-24 15:02:16 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "cyberpunk cat",
+      prompt: "cyberpunk dog",
     };
 
     const response = await env.AI.run(
